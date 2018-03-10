c52a69db4429cb69d97fa7e6c54b7df870b7a2e7
Matheus Sartori <ms.sartori@outlook.com.br>
2018-03-09 22:18:30 -0300

    Module Pattern no ConnectionFactory
    Aplicando o module pattern, agora temos uma única conexão para toda a aplicação, e "escondemos" o conteúdo do usuário.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,29 +1,44 @@
-var stores = ['negociacoes'];
-var version = 3;
-var dbName = 'negociacoes';
+var ConnectionFactory = (function() {
 
-class ConnectionFactory
-{
-    constructor()
-    {
-        throw new Error('Esta classe não pode ser instanciada. (Static class)');
+    var stores = ['negociacoes'];
+    var version = 3;
+    var dbName = 'negociacoes';
+
+    var connection = null;
+
+    return class ConnectionFactory {
+        constructor() {
+            throw new Error('Esta classe não pode ser instanciada. (Static class)');
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    if (!connection)
+                        connection = e.target.result;
+
+                    resolve(e.target.result);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
     }
-
-    static getConnection()
-    {
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
-            openRequest.onupgradeneeded = e => {
-
-            };
-
-            openRequest.onsuccess = e => {
-
-            };
-
-            openRequest.onerror = e => {
-
-            };
-        });
-    }
-}
+})();
