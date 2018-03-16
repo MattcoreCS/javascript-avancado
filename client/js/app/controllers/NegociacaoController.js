305f940a289242456c746d8afb0ca370b4c22a7b
Matheus Sartori <ms.sartori@outlook.com.br>
2018-03-16 14:59:42 -0300

    Update segurança

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -53,6 +53,10 @@ class NegociacaoController
         let service = new NegociacoesService();
         service
             .obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao =>
+                this._listaNegociacoes.negociacoes.indexOf(negociacao) == -1)
+            )
             .then(negociacoes => {
                 negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
                 this._mensagem.texto = 'Negociações do período importadas com sucesso';
