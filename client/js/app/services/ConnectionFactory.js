ab873f66cb2cf2ad81a63eb8e47343f639f0d1f1
Matheus Sartori <ms.sartori@outlook.com.br>
2018-03-26 20:04:44 -0300

    Transpiler com Babel: para navegadores antigos
    Atualização para o sistema rodar em navegadores mais antigos.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,59 +1,73 @@
-var ConnectionFactory = (function() {
+'use strict';
 
-    const stores = ['negociacoes'];
-    const version = 3;
-    const dbName = 'negociacoes';
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ConnectionFactory = function () {
+
+    var stores = ['negociacoes'];
+    var version = 3;
+    var dbName = 'negociacoes';
 
     var connection = null;
 
     var close = null;
 
-    return class ConnectionFactory {
-        constructor() {
+    return function () {
+        function ConnectionFactory() {
+            _classCallCheck(this, ConnectionFactory);
+
             throw new Error('Esta classe não pode ser instanciada. (Static class)');
         }
 
-        static getConnection() {
-            return new Promise((resolve, reject) => {
-                let openRequest = window.indexedDB.open(dbName, version);
-                openRequest.onupgradeneeded = e => {
-                    ConnectionFactory._createStores(e.target.result);
-                };
+        _createClass(ConnectionFactory, null, [{
+            key: 'getConnection',
+            value: function getConnection() {
+                return new Promise(function (resolve, reject) {
+                    var openRequest = window.indexedDB.open(dbName, version);
+                    openRequest.onupgradeneeded = function (e) {
+                        ConnectionFactory._createStores(e.target.result);
+                    };
 
-                openRequest.onsuccess = e => {
-                    if (!connection) {
-                        connection = e.target.result;
-                        close = connection.close.bind(connection);
-                        connection.close = function() {
-                          throw new Error('É impossível fechar a conexão.');
-                        };
-                    }
+                    openRequest.onsuccess = function (e) {
+                        if (!connection) {
+                            connection = e.target.result;
+                            close = connection.close.bind(connection);
+                            connection.close = function () {
+                                throw new Error('É impossível fechar a conexão.');
+                            };
+                        }
 
-                    resolve(e.target.result);
-                };
+                        resolve(e.target.result);
+                    };
 
-                openRequest.onerror = e => {
-                    console.log(e.target.error);
-                    reject(e.target.error.name);
-                };
-            });
-        }
-
-        static _createStores(connection) {
-            stores.forEach(store => {
-                if (connection.objectStoreNames.contains(store))
-                    connection.deleteObjectStore(store);
-
-                connection.createObjectStore(store, {autoIncrement: true});
-            });
-        }
-
-        static closeConnection()
-        {
-            if(connection) {
-                close();
-                connection = null;
+                    openRequest.onerror = function (e) {
+                        console.log(e.target.error);
+                        reject(e.target.error.name);
+                    };
+                });
             }
-        }
-    }
-})();
+        }, {
+            key: '_createStores',
+            value: function _createStores(connection) {
+                stores.forEach(function (store) {
+                    if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+
+                    connection.createObjectStore(store, { autoIncrement: true });
+                });
+            }
+        }, {
+            key: 'closeConnection',
+            value: function closeConnection() {
+                if (connection) {
+                    close();
+                    connection = null;
+                }
+            }
+        }]);
+
+        return ConnectionFactory;
+    }();
+}();
+//# sourceMappingURL=ConnectionFactory.js.map
